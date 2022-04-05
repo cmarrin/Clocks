f55b58548b5c6b1e9929f541877a1b25ef86388a
Chris Marrin <chris@marrin.com>
2022-04-05 15:53:42 -0700

    Initial xcodeproj

---
+++ mac/EtherClock/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  EtherClock
+//
+//  Created by Chris Marrin on 4/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ mac/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Clocks
+//
+//  Created by Chris Marrin on 4/4/22.
+//
+
+// based on https://stackoverflow.com/a/30269562
+// Minimal Pure C code to create a window in Cocoa
+
+// $ clang minimal.c -framework Cocoa -o minimal.app
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
