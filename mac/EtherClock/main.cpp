02ec24bca0bf08cf6b0592af93370c5305c9b626
Chris Marrin <chris@marrin.com>
2022-04-07 17:46:02 -0700

    First WordClock checkin
    Uses tigr to display bitmap with characters and then renders black boxes over characters to be dark. So far test shows some random time and weather just to show everything working

--- mac/EtherClock/main.cpp
+++ mac/EtherClock/main.cpp
@@ -7,8 +7,20 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include "tigr.h"
+
+int main(int argc, const char * argv[])
+{
+    Tigr* screen = tigrWindow(800, 800, "Hello", TIGR_AUTO);
+    Tigr* words = tigrLoadImage("words.png");
+
+    while (!tigrClosed(screen))
+    {
+        tigrClear(screen, tigrRGB(0xa0, 0x40, 0x40));
+        tigrFillRect(screen, 300, 200, 50, 50, tigrRGBA(0x00, 0x00, 0x00, 0xc0));
+        tigrUpdate(screen);
+    }
+    tigrFree(screen);
+
     return 0;
 }
---
+++ mac/tigr/tigr_internal.h
@@ -0,0 +1,80 @@
+// can't use pragma once here because this file probably will endup in .c
+#ifndef __TIGR_INTERNAL_H__
+#define __TIGR_INTERNAL_H__
+
+#include "tigr.h"
+
+// Graphics configuration.
+#define TIGR_GAPI_GL
+
+// Creates a new bitmap, with extra payload bytes.
+Tigr* tigrBitmap2(int w, int h, int extra);
+
+// Resizes an existing bitmap.
+void tigrResize(Tigr* bmp, int w, int h);
+
+// Calculates the biggest scale that a bitmap can fit into an area at.
+int tigrCalcScale(int bmpW, int bmpH, int areaW, int areaH);
+
+// Calculates a new scale, taking minimum-scale flags into account.
+int tigrEnforceScale(int scale, int flags);
+
+// Calculates the correct position for a bitmap to fit into a window.
+void tigrPosition(Tigr* bmp, int scale, int windowW, int windowH, int out[4]);
+
+// ----------------------------------------------------------
+
+#include "TargetConditionals.h"
+#define __MACOS__ 1
+
+#ifdef TIGR_GAPI_GL
+#define GL_SILENCE_DEPRECATION
+#include <OpenGL/gl3.h>
+
+typedef struct {
+    void* glContext;
+    GLuint tex[2];
+    GLuint vao;
+    GLuint program;
+    GLuint uniform_projection;
+    GLuint uniform_model;
+    GLuint uniform_parameters;
+    int gl_legacy;
+    int gl_user_opengl_rendering;
+} GLStuff;
+#endif
+
+#define MAX_TOUCH_POINTS 10
+
+typedef struct {
+    int shown, closed;
+#ifdef TIGR_GAPI_GL
+    GLStuff gl;
+#endif
+
+    Tigr* widgets;
+    int widgetsWanted;
+    unsigned char widgetAlpha;
+    float widgetsScale;
+
+    float p1, p2, p3, p4;
+
+    int flags;
+    int scale;
+    int pos[4];
+    int lastChar;
+    char keys[256], prev[256];
+    int mouseInView;
+    int mouseButtons;
+} TigrInternal;
+// ----------------------------------------------------------
+
+TigrInternal* tigrInternal(Tigr* bmp);
+
+void tigrGAPICreate(Tigr* bmp);
+void tigrGAPIDestroy(Tigr* bmp);
+int tigrGAPIBegin(Tigr* bmp);
+int tigrGAPIEnd(Tigr* bmp);
+void tigrGAPIPresent(Tigr* bmp, int w, int h);
+
+#endif
