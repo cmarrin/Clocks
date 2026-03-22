385891b8e2ce7ef395b5ae0c489232e3efde8f74
Chris Marrin <chris@marrin.com>
2026-03-22 13:37:27 -0700

    Changed range of brightness for OfficeClock

--- OfficeClock/OfficeClock.h
+++ OfficeClock/OfficeClock.h
@@ -18,14 +18,20 @@ static constexpr const char* Hostname = "officeclock";
 static constexpr const char* Version = "4.0";
 
 // Display related
+// Range for the light sensor on the ESP23C6 was measured at 3200 in 
+// a dark room and 200 with a light shining on the sensor. This is
+// an inverted range so we invert it before applying the min and max. Also
+// The incoming value is scaled to 10 bits, so the min and max
+// values are based on that.
+
 static constexpr uint32_t StartupScrollRate = 50;
 static constexpr uint32_t DateScrollRate = 50;
 static constexpr uint8_t SelectButton = 14;
 static constexpr uint32_t LightSensor = 0;
 static constexpr uint32_t NumberOfBrightnessLevels = 31;
 static constexpr bool InvertAmbientLightLevel = true;
-static constexpr uint32_t MinLightSensorLevel = 50;
-static constexpr uint32_t MaxLightSensorLevel = 900;
+static constexpr uint32_t MinLightSensorLevel = 200; // based on a 10 bit (scaled) value
+static constexpr uint32_t MaxLightSensorLevel = 950; // based on a 10 bit (scaled) value
 static constexpr uint32_t DoneTimeDuration = 100;
 
 class OfficeClock : public mil::Application
